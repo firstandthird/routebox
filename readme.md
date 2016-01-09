3839f5255dafca1d6deeab304a05e63911672482
Connor Peet <connor@peet.io>
2016-01-09 14:32:05 -0500

    init

---
+++ readme.md
@@ -0,0 +1,28 @@
+# Routebox
+
+[![Build Status](https://img.shields.io/travis/WatchBeam/routebox.svg?style=flat-square)](https://travis-ci.org/WatchBeam/routebox)
+
+Routebox integrates with catbox to provide transparent route-level caching. It can work with zero configuration changes on your server.
+
+## Usage
+
+To Routebox, simply register it on your server.
+
+```js
+server.register(require('routebox'), function (err) {
+    // ...
+});
+```
+
+By default, all endpoints with the `cache` configured in their [route options](http://hapijs.com/api#route-options) and `privacy` set to `public` (or omitted; public is the default) will be cached. Routebox automatically hooks in to the `expiresAt`, `expiresIn`, and `statuses` options of the caching config.
+
+These options are available when routebox is registered and can also be overridden on a per-route basis by passing in `config.plugins.routebox`:
+
+ * `cache` corresponds to the cache name for response caches. Uses the server's default if not given.
+ * `enabled` whether to enable caching on the endpoint. Defaults to `true`, meaning all viable (see above) endpoints will be cached.
+ * `digest` defaults to `djb2`, this is the algorithm used to digest the request for caching purposes. Other available options are: `md5`, `sha1`, `sha256`, `sha512`, `ripemd160`.
+ * `segment` is the Catbox cache segment to store in. Defaults to `routebox`
+ * `parse` configures which parts of the request will be used to form the cache key:
+    * `query` whether to include the query string. Defaults to `true`.
+    * `method` whether to include the request method. Defaults to `true`.
+    * `path` whether to include the route path. Defaults to `true`.
---
+++ test/routebox.test.js
@@ -0,0 +1,138 @@
+const Hapi = require('hapi');
+const expect = require('chai').expect;
+const sinon = require('sinon');
+
+describe('routebox', function () {
+    var server;
+    var clock;
+    beforeEach(function (done) {
+        clock = sinon.useFakeTimers();
+        server = new Hapi.Server();
+        server.connection();
+        server.register(require('../'), (err) => {
+            expect(err).to.not.exist;
+            server.start(done);
+        });
+    });
+
+    afterEach(function (done) {
+        server.stop(done);
+        clock.restore();
+    });
+
+    it('caches responses', function (done) {
+        server.route({
+            method: 'get', path: '/a',
+            config: {
+                cache: { expiresIn: 1000 },
+                handler: (req, reply) => reply(i++),
+            },
+        });
+
+        var i = 0;
+        server.inject({ method: 'GET', url: '/a' }, (res) => {
+            expect(res.result).to.equal(0);
+            expect(res.statusCode).to.equal(200);
+
+            server.inject({ method: 'GET', url: '/a' }, (res2) => {
+                expect(res2.result).to.equal(0);
+                expect(res2.statusCode).to.equal(200);
+                done();
+            });
+        });
+    });
+
+    it('expires ttl correctly', function (done) {
+        server.route({
+            method: 'get', path: '/a',
+            config: {
+                cache: { expiresIn: 1000 },
+                handler: (req, reply) => reply(i++),
+            },
+        });
+
+        var i = 0;
+        server.inject({ method: 'GET', url: '/a' }, (res) => {
+            expect(res.result).to.equal(0);
+            expect(res.statusCode).to.equal(200);
+            clock.tick(1001);
+
+            server.inject({ method: 'GET', url: '/a' }, (res2) => {
+                expect(res2.result).to.equal(1);
+                expect(res2.statusCode).to.equal(200);
+                done();
+            });
+        });
+    });
+
+    it('does not cache on routes without caching', function (done) {
+        server.route({
+            method: 'get', path: '/a',
+            config: {
+                handler: (req, reply) => reply(i++),
+            },
+        });
+
+        var i = 0;
+        server.inject({ method: 'GET', url: '/a' }, (res) => {
+            expect(res.result).to.equal(0);
+            expect(res.statusCode).to.equal(200);
+
+            server.inject({ method: 'GET', url: '/a' }, (res2) => {
+                expect(res2.result).to.equal(1);
+                expect(res2.statusCode).to.equal(200);
+                done();
+            });
+        });
+    });
+
+    it('does not cache on routes with private caching', function (done) {
+        server.route({
+            method: 'get', path: '/a',
+            config: {
+                cache: { expiresIn: 1000, privacy: 'private' },
+                handler: (req, reply) => reply(i++),
+            },
+        });
+
+        var i = 0;
+        server.inject({ method: 'GET', url: '/a' }, (res) => {
+            expect(res.result).to.equal(0);
+            expect(res.statusCode).to.equal(200);
+
+            server.inject({ method: 'GET', url: '/a' }, (res2) => {
+                expect(res2.result).to.equal(1);
+                expect(res2.statusCode).to.equal(200);
+                done();
+            });
+        });
+    });
+
+    it('does not cache not-ok responses', function (done) {
+        server.route({
+            method: 'get', path: '/a',
+            config: {
+                cache: { expiresIn: 1000, privacy: 'private' },
+                handler: (req, reply) => {
+                    i++;
+                    if (i === 1) {
+                        reply(new Error());
+                    } else {
+                        reply(i);
+                    }
+                },
+            },
+        });
+
+        var i = 0;
+        server.inject({ method: 'GET', url: '/a' }, (res) => {
+            expect(res.statusCode).to.equal(500);
+
+            server.inject({ method: 'GET', url: '/a' }, (res2) => {
+                expect(res2.result).to.equal(2);
+                expect(res2.statusCode).to.equal(200);
+                done();
+            });
+        });
+    });
+});
